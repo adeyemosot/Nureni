bcb5a5245277f8201f3b30cfdb84d7a3d52d4820
adeyemosot <31286906+adeyemosot@users.noreply.github.com>
2017-09-19 21:41:37 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<?php
+echo file_get_contents("http://api.openweathermap.org/data/2.5/find?q=calabar,ng&type=accurate&mode=jso‌​n&appid=6f54a50699d2f2c76e02e2dd960b2946");
+?>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<style type="text/css">
+ input{
+ border:1px solid olive;
+ border-radius:5px;
+ }
+ h1{
+  color:darkgreen;
+  font-size:22px;
+  text-align:center;
+ }
+</style>
+</head>
+<body>
+<h1>Login<h1>
+<form action='' method='post'>
+<table cellspacing='5' align='center'>
+<tr><td>User name:</td><td><input type='text' name='name'/></td></tr>
+<tr><td>Password:</td><td><input type='password' name='pwd'/></td></tr>
+<tr><td></td><td><input type='submit' name='submit' value='Submit'/></td></tr>
+</table>
+
+</form>
+<?php
+
+if ($_POST['name'] == "adekunle" && $_POST['pwd'] == "adeyemo")
+{
+ 
+	header('location:index.php');
+ 
+ else{
+
+    echo'Please enter correct username and password';
+   }
+ 
+ }
+
+?>
+</body>
+</html>
